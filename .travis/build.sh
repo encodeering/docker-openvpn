6fac2f5af81ce3b56fc2727bb19b4c05a4797c64
Michael Clausen <encodeering@gmail.com>
2016-01-06 18:56:01 +0100

    container should be executed once
    * and print the openvpn version; can be seen as verification and debug/audit step
    
    NOTE
    
    * openvpn returns exit code 1 for --version; we treat the sales information from the output as criteria, which must be non empty then
    ** sub process doesn't seem to work, e.g tee >(grep "sales@openvpn.net" > .version)

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,3 +11,9 @@ docker tag -f "$REPOSITORY/debian-$ARCH:jessie" "debian:jessie"
 patch -p0 --no-backup-if-mismatch --directory=$PROJECT < .patch/Dockerfile.patch
 
 docker build -t "$TAG:$TAGSPECIFIER" "$PROJECT"
+
+set +e
+
+docker run --rm "$TAG:$TAGSPECIFIER" openvpn --version | tee .version
+
+grep -qF -- "<sales@openvpn.net>" .version
