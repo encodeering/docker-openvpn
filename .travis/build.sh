cd3f8edf42b94a42bd63d29140e41741cfa587d5
Michael Clausen <encodeering@gmail.com>
2016-01-06 14:22:36 +0100

    creates a debian based openvpn image
    * arch: armhf, amd64
    * version: 1.1.0
    
    NOTE
    
    * kylemanna/docker-openvpn switched to alpine, last debian branch has been tagged with 1.1.0
    ** need to verify if armhf alpine build is working now; encountered some problems with pki setup on arm
    
    * ca-certificates should be installed (newest version) as its required by curl

---
+++ .travis/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+TAG="$REPOSITORY/$PROJECT-$ARCH"
+TAGSPECIFIER="$VERSION"
+
+docker pull   "$REPOSITORY/debian-$ARCH:jessie"
+docker tag -f "$REPOSITORY/debian-$ARCH:jessie" "debian:jessie"
+
+patch -p0 --no-backup-if-mismatch --directory=$PROJECT < .patch/Dockerfile.patch
+
+docker build -t "$TAG:$TAGSPECIFIER" "$PROJECT"
