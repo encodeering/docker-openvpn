7facfa32e30c854a5dd32eb24b56a9ca2264fe94
Michael Clausen <encodeering@gmail.com>
2018-01-02 14:57:06 +0100

    switches to ci package
    * taking the latest release of 0.1.4

---
+++ modules/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# configuration
+#   env.ARCH
+#   env.PROJECT
+#   env.VERSION
+#   env.REPOSITORY
+
+set -e
+
+import com.encodeering.docker.config
+import com.encodeering.docker.docker
+
+docker-pull "$REPOSITORY/debian-$ARCH:jessie" "debian:jessie"
+
+patch -p0 --no-backup-if-mismatch --directory=$PROJECT < patch/Dockerfile.patch
+
+docker build -t "$DOCKER_IMAGE" "$PROJECT"
+
+set +e
+
+docker run --rm "$DOCKER_IMAGE" openvpn --version | tee .version
+
+grep -qF -- "<sales@openvpn.net>" .version
