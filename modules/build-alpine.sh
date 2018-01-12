3b72fd32927ad95a19c73fe3b910b209485b73eb
Michael Clausen <encodeering@gmail.com>
2018-01-12 09:59:34 +0100

    updates to latest revision
    * openvpn branch seems to be stale

--- modules/build-alpine.sh
+++ modules/build-alpine.sh
@@ -5,6 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:3.6"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.7" "alpine:latest"
 
 docker-build "$PROJECT"
